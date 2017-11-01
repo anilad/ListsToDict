a497bb4d6a721226691615ac3ca647c18658a04e
anilad <aniladdao@gmail.com>
2017-10-31 17:28:42 -0700

    made edits to index.py

--- index.py
+++ index.py
@@ -1,28 +1,5 @@
 # Create a function that takes in two lists and creates a single dictionary where the first list contains keys and the second values. 
 # Assume the lists will be of equal length.
-'''
-def make_dict(arr1, arr2):
-    new_dict = {}
-    value=0
-    if(len(arr1)>len(arr2)):
-        for key in arr1:
-            if value == len(arr2):
-                new_dict[key]= " "
-            else:
-                new_dict[key]=arr2[value]
-                value+=1
-            
-        return new_dict
-    else:
-        for key in arr1:
-            if value == len(arr2):
-                new_dict[key]= " "
-            else:
-                new_dict[key]=arr2[value]
-                value+=1
-'''
-
-
 
 
 name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
