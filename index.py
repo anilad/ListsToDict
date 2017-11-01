fa517979982eb28b2795d685496527af66127f86
anilad <aniladdao@gmail.com>
2017-10-31 17:42:04 -0700

    changed function to keep the last item in the longer array and assign a dummy value

--- index.py
+++ index.py
@@ -3,10 +3,24 @@
 
 
 name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
-favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks"]
 def make_dict(arr1,arr2):
-    mynewdict = zip(arr1,arr2)
-    newestdict = dict(mynewdict)
-    return newestdict
+    newDict = {}
+    value =0
+    if len(arr1)>len(arr2):
+        for val in arr1:
+            if value <len(arr2):
+                newDict[val]=arr2[value]
+            else:
+                newDict[val]="none"
+            value +=1
+    else:
+        for val in arr2:
+            if value <len(arr1):
+                newDict[val]=arr1[value]
+            else:
+                newDict[val]="none"
+            value +=1
+    return newDict
 
 print make_dict(name,favorite_animal)
